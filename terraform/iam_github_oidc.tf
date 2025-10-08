d86bcffdd8ffc3fc01c2969ae59f7065355da1b1
btso8 <btso8@protonmail.com>
2025-10-08 15:31:19 +0100

    bugfix

--- terraform/iam_github_oidc.tf
+++ terraform/iam_github_oidc.tf
@@ -17,6 +17,12 @@ data "aws_iam_policy_document" "github_oidc_trust" {
       variable = "token.actions.githubusercontent.com:aud"
       values   = ["sts.amazonaws.com"]
     }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:${var.github_owner}/${var.github_repo}:*"]
+    }
   }
 }
 
